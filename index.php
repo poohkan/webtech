04a63fd913d1f230b14d7de0b14e76fd95291c63
poohkan <poohbah.w@gmail.com>
2020-12-15 16:12:55 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -10,8 +10,9 @@
         <p>ชื่อหลักสูตรภาษาไทย: หลักสูตรวิทยาศาสตรบัณฑิต สาขาวิชาวิทยาการข้อมูล</p>
         <p>ภาษาอังกฤษ: Bachelor of Science Program in Data Science</p>
 
-        <a href="https://www.cp.su.ac.th/curricula/Bachelor-of-Science-Program-in-Data-Science">รายละเอียดหลักสูตร วิทยาการข้อมูล</a>
-        <img src="https://sites.google.com/site/funnycatmeawww/_/rsrc/1422326075261/home/6997052-funny-cat.jpg" width="500" height="400">
+        <a href="https://www.cp.su.ac.th/curricula/Bachelor-of-Science-Program-in-Data-Science">รายละเอียดหลักสูตร วิทยาการข้อมูล
+        <br>
+        <img src="https://sites.google.com/site/funnycatmeawww/_/rsrc/1422326075261/home/6997052-funny-cat.jpg" width="100" height="80"></a>
 
     </body>
 </html>
